b73198e04ee967efff540a0e07e3159c4baecfb0
WilliamHeidemann <williamskou2227@gmail.com>
2022-06-18 09:57:48 +0200

    Rope v1

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,12 +6,14 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [Range(0.1f, 100f)]
-    [SerializeField] private float speed;
+    [SerializeField] private float moveSpeed;
+    [Range(0.1f, 10f)]
+    [SerializeField] private float rotationSpeed;
     private float rotation;
 
     private void Update()
     {
-        rotation = Input.GetAxis("Horizontal");
+        rotation = Input.GetAxis("Horizontal") * rotationSpeed;
         transform.Rotate(new Vector3(0, rotation, 0));
 
         if (Input.GetKeyDown(KeyCode.Space))
@@ -22,6 +24,6 @@ public class Movement : MonoBehaviour
 
     private void LateUpdate()
     {
-        rb.MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
+        rb.MovePosition(transform.position + transform.forward * Time.deltaTime * moveSpeed);
     }
 }

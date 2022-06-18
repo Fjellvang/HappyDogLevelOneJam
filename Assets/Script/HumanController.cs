7678f8a145946cfa0004e6ce902a569ce07c708b
aleksander fjellvang <asf@appstract.dk>
2022-06-18 17:01:49 +0200

    set anchor on dog

--- Assets/Script/HumanController.cs
+++ Assets/Script/HumanController.cs
@@ -3,6 +3,7 @@ using Assets.Script.States.HumanStates;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HumanController : MonoBehaviour
 {
@@ -12,11 +13,16 @@ public class HumanController : MonoBehaviour
     [HideInInspector]
     public HumanStateMachine stateMachine;
     // Start is called before the first frame update
+
+    public Transform Goal;
+
     private void OnEnable()
     {
         stateMachine = new HumanStateMachine(this);
         rigidBody = GetComponent<Rigidbody>();
         stateMachine.TransitionState(HumanBaseState.WalkingState);
+
+        GetComponent<NavMeshAgent>().destination = Goal.position;
     }
 
     // Update is called once per frame

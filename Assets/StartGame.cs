bf0f73784e7fef1b9d56676324b30e2d23827912
WilliamHeidemann <williamskou2227@gmail.com>
2022-06-19 10:50:26 +0200

    Update

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,17 @@ using UnityEngine;
 public class StartGame : MonoBehaviour
 {
     [SerializeField] private Animator anim;
-
+    [SerializeField] private GameObject startButton;
     public void StartGameScene()
     {
+        StartCoroutine(PlayAnimation());       
+    }
+
+    private IEnumerator PlayAnimation()
+    {
         anim.SetTrigger("Start");
+        startButton.SetActive(false);
+        yield return new WaitForSeconds(8f);
+        gameObject.SetActive(false);
     }
 }

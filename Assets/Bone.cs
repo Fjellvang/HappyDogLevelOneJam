c875dee4be85eb3e6aeb0ae446f874d44785a5da
WilliamHeidemann <williamskou2227@gmail.com>
2022-06-19 12:53:46 +0200

    Adjustment

--- Assets/Bone.cs
+++ Assets/Bone.cs
@@ -6,12 +6,20 @@ public class Bone : MonoBehaviour
 {
     [SerializeField] private IntVariable bonesCollected;
     [SerializeField] private AudioSource celebrationSound;
+    private SphereCollider sphereCollider;
+
+    private void Start()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Dog")
         {
             celebrationSound.Play();
             bonesCollected.Value += 1;
+            sphereCollider.enabled = false;
             transform.GetChild(0).gameObject.SetActive(false);
             Destroy(gameObject, 4f);
         }

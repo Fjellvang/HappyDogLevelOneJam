de48878f11a9ea16425a3cbe1c337aac6e6308e6
WilliamHeidemann <williamskou2227@gmail.com>
2022-06-19 11:42:01 +0200

    Changes

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -7,6 +7,8 @@ public class StartGame : MonoBehaviour
 {
     [SerializeField] private Animator anim;
     [SerializeField] private GameObject startButton;
+    [SerializeField] private GameObject humanAndDog;
+    [SerializeField] private GameObject cinemachineGameObject;
     public void StartGameScene()
     {
         StartCoroutine(PlayAnimation());       
@@ -17,6 +19,8 @@ public class StartGame : MonoBehaviour
         anim.SetTrigger("Start");
         startButton.SetActive(false);
         yield return new WaitForSeconds(8f);
+        humanAndDog.SetActive(true);
         gameObject.SetActive(false);
+        cinemachineGameObject.SetActive(true);
     }
 }

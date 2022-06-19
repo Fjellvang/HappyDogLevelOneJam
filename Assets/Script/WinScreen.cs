8b4b17a6aabeddfefdb105eb995200cab9325d36
WilliamHeidemann <williamskou2227@gmail.com>
2022-06-19 13:54:32 +0200

    fadeanimation

--- Assets/Script/WinScreen.cs
+++ Assets/Script/WinScreen.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinScreen : MonoBehaviour
 {
     [SerializeField] private IntVariable dogExcitement;
     [SerializeField] private IntVariable bonesCollected;
     [SerializeField] private GameObject winScreen;
+    [SerializeField] private Image image;
 
     private void Update()
     {
-        if(dogExcitement.Value >= 100 && bonesCollected.Value == 10)
+        if(dogExcitement.Value >= 0 && bonesCollected.Value == 1)
         {
             winScreen.SetActive(true);
+            image.color = new Color(1, 1, 1, 0);
+            StartCoroutine(FadeIn());
+        }
+    }
+
+    private IEnumerator FadeIn()
+    {
+        for (float i = 0; i <= 3f; i += Time.deltaTime)
+        {
+            // set color with i as alpha
+            image.color = new Color(1, 1, 1, i);
+            yield return null;
         }
     }
 }
